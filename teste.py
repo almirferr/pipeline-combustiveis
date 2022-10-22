f74e733bf9d240d4cdb41e612a4068c363f329e0
almirferr <78576985+almirferr@users.noreply.github.com>
2022-10-22 09:22:31 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -8,6 +8,7 @@ SparkConf()
     .set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
     .set('spark.hadoop.fs.s3a.aws.credentials.provider', 'com.amazonaws.auth.EnvironmentVariableCredentialsProvider')
     .set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:2.7.3')
+    .set("spark.hadoop.fs.s3a.endpoint", "s3.us-east-2.amazonaws.com")
 )
 
 # apply config

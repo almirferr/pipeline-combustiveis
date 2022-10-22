606ee6ccd31db901fa05b9f260c145a432c7cdfa
almirferr <78576985+almirferr@users.noreply.github.com>
2022-10-22 10:07:19 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -32,10 +32,7 @@ if __name__ == "__main__":
     print("*****************")
     print("Iniciando!!!")
     print("*****************")
-  
 
-    
-    df.printSchema()
     
     print("finalizado!")
 

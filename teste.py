446b163af26cc50b736bc17f96cb737d585f1ce9
almirferr <78576985+almirferr@users.noreply.github.com>
2022-10-24 20:07:46 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,48 +1,45 @@
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 
-print("*****************")
-print("Iniciando!!!")
-print("*****************")
-
-aws_access_key_id = "AKIAYBEEQYFSDKNFEHG7"
-aws_secret_access_key = "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"
-
-print("********aws_access_key_id*********")
-print(aws_access_key_id)
-print("*****************")
-
-conf = SparkConf()
-conf.set("spark.hadoop.fs.s3a.impl","org.apache.hadoop.fs.s3a.S3AFileSystem")
-conf.set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:2.7.3,org.apache.hadoop:hadoop-common:2.7.3')
-conf.set("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.BasicAWSCredentialsProvider")
-conf.set("spark.hadoop.fs.s3a.access.key", aws_access_key_id)
-conf.set("spark.hadoop.fs.s3a.secret.key", aws_secret_access_key)
-#conf.set("spark.hadoop.fs.s3a.endpoint", "s3.us-east-2.amazonaws.com")
-#conf.set("com.amazonaws.services.s3.enableV4", "true")
-
-print("versao 4")
-
-# apply config
-sc = SparkContext(conf=conf).getOrCreate()
-
 if __name__ == "__main__":
-    
+
     # init spark session
     spark = SparkSession\
             .builder\
             .appName("Repartition Job")\
             .getOrCreate()
-    
-    print("setting")
-    
+
     spark.sparkContext.setLogLevel("WARN")
 
-    print("reading")
-    
-    df = spark.read.csv("s3a://almirbf-teste2/landing/titanic.csv")
+    print("criando contexto!")
 
-    df.show(false)
-    df.printSchema()
+    conf = spark.sparkContext._jsc.hadoopConfiguration()
+    conf.set("fs.gs.auth.service.account.enable", "true")
+    conf.set("fs.gs.auth.service.account.json.keyfile", "/mnt/secrets/key.json")
+    conf.set("fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem")
+    conf.set("fs.AbstractFileSystem.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS")    
 
-    print("finishing")
+    print("*****************")
+    print("Iniciando!!!")
+    print("*****************")
+
+    print("hadoopConfiguration!")
+    spark._jsc.hadoopConfiguration().set("google.cloud.auth.service.account.json.keyfile","c:\gcp-secret-keys.json")
+
+    print("read csv gs!")
+    df=spark.read.csv(f"gs://landing-zone-202210182226/titanic.csv", header=True, sep=";")
+    df.show()
+
+    print("write gs!")
+    df.write.mode("overwrite").format("parquet").save(f"gs://silver-zone-202210182226/parquet/") 
+
+    print("read parquet gs!")
+    df2=spark.read.parquet(f"gs://silver-zone-202210182226/parquet")
+
+    df2.show()
+
+    print("*****************")
+    print("Escrito com sucesso!")
+    print("*****************")
+
+    spark.stop()

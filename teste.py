d6f4f28a9bffd52034a5fe1d95c81f4c2f278544
almirferr <78576985+almirferr@users.noreply.github.com>
2022-10-24 20:09:43 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -13,30 +13,30 @@ if __name__ == "__main__":
 
     print("criando contexto!")
 
-    conf = spark.sparkContext._jsc.hadoopConfiguration()
-    conf.set("fs.gs.auth.service.account.enable", "true")
-    conf.set("fs.gs.auth.service.account.json.keyfile", "/mnt/secrets/key.json")
-    conf.set("fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem")
-    conf.set("fs.AbstractFileSystem.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS")    
+#    conf = spark.sparkContext._jsc.hadoopConfiguration()
+#    conf.set("fs.gs.auth.service.account.enable", "true")
+#    conf.set("fs.gs.auth.service.account.json.keyfile", "/mnt/secrets/key.json")
+#    conf.set("fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem")
+#    conf.set("fs.AbstractFileSystem.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS")    
 
     print("*****************")
     print("Iniciando!!!")
     print("*****************")
 
     print("hadoopConfiguration!")
-    spark._jsc.hadoopConfiguration().set("google.cloud.auth.service.account.json.keyfile","c:\gcp-secret-keys.json")
+#    spark._jsc.hadoopConfiguration().set("google.cloud.auth.service.account.json.keyfile","c:\gcp-secret-keys.json")
 
     print("read csv gs!")
-    df=spark.read.csv(f"gs://landing-zone-202210182226/titanic.csv", header=True, sep=";")
-    df.show()
+#    df=spark.read.csv(f"gs://landing-zone-202210182226/titanic.csv", header=True, sep=";")
+#    df.show()
 
     print("write gs!")
-    df.write.mode("overwrite").format("parquet").save(f"gs://silver-zone-202210182226/parquet/") 
+#    df.write.mode("overwrite").format("parquet").save(f"gs://silver-zone-202210182226/parquet/") 
 
     print("read parquet gs!")
-    df2=spark.read.parquet(f"gs://silver-zone-202210182226/parquet")
+#    df2=spark.read.parquet(f"gs://silver-zone-202210182226/parquet")
 
-    df2.show()
+#    df2.show()
 
     print("*****************")
     print("Escrito com sucesso!")
